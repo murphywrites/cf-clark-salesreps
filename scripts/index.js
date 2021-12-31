43f26dc1738f930c1e028171dea039a36f9fdd46
Michael Murphy <mike@murphyscode.com>
2021-12-31 11:49:16 -0600

    initial upload

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+import React from 'https://cdn.skypack.dev/react';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+
+import App from './components/App.js';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
