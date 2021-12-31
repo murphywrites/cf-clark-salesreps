3154102aaee2fe1d9ce0e51f37bdd1ed2ee487f1
Michael Murphy <mike@murphyscode.com>
2021-12-31 18:52:31 +0000

    delete all

--- scripts/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'https://cdn.skypack.dev/react';
-
-class App extends React.Component {
-  render(){
-    return <div>Hello World!</div>;
-  }
-}
-
-export default App;
--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'https://cdn.skypack.dev/react';
-import ReactDOM from 'https://cdn.skypack.dev/react-dom';
-
-import App from './components/App.js';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
